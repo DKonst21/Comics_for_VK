91b74fcdc2125a3cd606b8549a63390493292492
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-03-19 00:18:07 +0400

    Added protection for GET parameters. Corrected procedure names. Dried out the code.

--- main.py
+++ main.py
@@ -1,80 +1,78 @@
 import requests
 import os
 import random
-from download_comics import download_comics
+from download_comic import download_comic
 from dotenv import load_dotenv
 
 
-def get_comics_photo():
-
-    number_comics = str(random.randrange(1, 2750, 1))
-    response = requests.get(f'https://xkcd.com/{number_comics}/info.0.json')
+def get_comic_photo(number_last_comic):
+    number_comic = random.randrange(1, number_last_comic, 1)
+    response = requests.get(f'https://xkcd.com/{number_comic}/info.0.json')
     response.raise_for_status()
     return response.json()
 
 
 def get_response_api_vk(access_token):
-
-    response = requests.get(f"https://api.vk.com/method/groups.get?PARAMS&access_token={access_token}&v=5.131")
+    payload = {"PARAMS": "", "access_token": f"{access_token}", "v": "5.131"}
+    response = requests.get('https://api.vk.com/method/groups.get', params=payload)
     response.raise_for_status()
     return response.json()
 
 
-def get_wall_upload_server(access_token, group_id):
-
-    response = requests.get(f"https://api.vk.com/method/photos.getWallUploadServer?group_id={group_id}"
-                            f"&access_token={access_token}&v=5.131")
+def get_address_for_download_photo(access_token, group_id):
+    payload = {"group_id": f"{group_id}", "access_token": f"{access_token}", "v": "5.131"}
+    response = requests.get('https://api.vk.com/method/photos.getWallUploadServer', params=payload)
     response.raise_for_status()
     return response.json()['response']
 
 
-def uploading_photos_to_server(access_token, group_id):
-
+def upload_photos_to_server(access_token, group_id):
     with open('image.png', 'rb') as file:
-        url = get_wall_upload_server(access_token, group_id)['upload_url']
+        url = get_address_for_download_photo(access_token, group_id)['upload_url']
         files = {
             'photo': file,
         }
         response = requests.post(url, files=files)
-        response.raise_for_status()
-        server = response.json()['server']
-        photo = response.json()['photo']
-        hash_photo = response.json()['hash']
         file.close()
-        return save_wall_photo(access_token, group_id, server, photo, hash_photo)
+        response.raise_for_status()
+        uploading_photo = [response.json()['server'], response.json()['photo'], response.json()['hash']]
+        return uploading_photo
 
 
-def save_wall_photo(access_token, group_id, server, photo, hash_photo):
-
-    response = requests.post(f"https://api.vk.com/method/photos.saveWallPhoto?group_id={group_id}&photo={photo}"
-                             f"&server={server}&hash={hash_photo}&access_token={access_token}&v=5.131")
+def save_wall_photo(access_token, group_id, uploading_photo):
+    payload = {"group_id": f"{group_id}", "photo": f"{uploading_photo[1]}", "server": f"{uploading_photo[0]}",
+               "hash": f"{uploading_photo[2]}", "access_token": f"{access_token}", "v": "5.131"}
+    response = requests.post('https://api.vk.com/method/photos.saveWallPhoto', params=payload)
     response.raise_for_status()
     return response.json()['response'][0]
 
 
-def post_comic_on_wall(access_token, group_id, message):
-
-    response_server = uploading_photos_to_server(access_token, group_id)
-    owner_id = str(response_server['owner_id'])
-    from_group = True
-    media_id = str(response_server['id'])
+def post_comic_on_wall(access_token, group_id, response_server, message):
+    owner_id = response_server['owner_id']
+    media_id = response_server['id']
     attachments = f'photo{owner_id}_{media_id}'
-    response = requests.post(f"https://api.vk.com/method/wall.post?owner_id={-group_id}&group_id={group_id}"
-                             f"&from_group={from_group}&attachments={attachments}&message={message}"
-                             f"&access_token={access_token}&v=5.131")
+    payload = {"owner_id": f"{-group_id}", "group_id": f"{group_id}", "from_group": True,
+               "attachments": f"{attachments}", "message": f"{message}", "access_token": f"{access_token}",
+               "v": "5.131"}
+    response = requests.post('https://api.vk.com/method/wall.post', params=payload)
     response.raise_for_status()
     return response.json()
 
 
 def main():
     load_dotenv()
-    access_token = os.environ['ACCESS_TOKEN_VK']
-    group_id = int(os.environ['GROUP_ID'])
-    image = get_comics_photo()
-
-    get_response_api_vk(access_token)
-    download_comics(image['img'], 'image.png')
-    post_comic_on_wall(access_token, group_id, image['alt'])
+    access_token = os.environ['VK_ACCESS_TOKEN']
+    group_id = int(os.environ['VK_GROUP_ID'])
+    number_last_comic = 2750
+    comic_info = get_comic_photo(number_last_comic)
+    try:
+        get_response_api_vk(access_token)
+        download_comic(comic_info['img'], 'image.png')
+        uploading_photo = upload_photos_to_server(access_token, group_id)
+        save_photo = save_wall_photo(access_token, group_id, uploading_photo)
+        post_comic_on_wall(access_token, group_id, save_photo, comic_info['alt'])
+    finally:
+        print("Comics uploaded to the wall of the VK group!")
     os.remove('image.png')
 
 
