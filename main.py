9094606ed6594211f3a6dffb57eb0d03ca2e47a6
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-03-20 17:31:41 +0400

    delete function get_response_api_vk. Dried out the code.

--- main.py
+++ main.py
@@ -12,48 +12,38 @@ def get_comic_photo(number_last_comic):
     return response.json()
 
 
-def get_response_api_vk(access_token):
-    payload = {"PARAMS": "", "access_token": f"{access_token}", "v": "5.131"}
-    response = requests.get('https://api.vk.com/method/groups.get', params=payload)
-    response.raise_for_status()
-    return response.json()
-
-
-def get_address_for_download_photo(access_token, group_id):
-    payload = {"group_id": f"{group_id}", "access_token": f"{access_token}", "v": "5.131"}
+def get_address_for_upload_photo(access_token, group_id, version):
+    payload = {"group_id": {group_id}, "access_token": {access_token}, "v": {version}}
     response = requests.get('https://api.vk.com/method/photos.getWallUploadServer', params=payload)
     response.raise_for_status()
-    return response.json()['response']
+    return response.json()['response']['upload_url']
 
 
-def upload_photos_to_server(access_token, group_id):
+def upload_photos_to_server(access_token, group_id, version):
     with open('image.png', 'rb') as file:
-        url = get_address_for_download_photo(access_token, group_id)['upload_url']
+        url = get_address_for_upload_photo(access_token, group_id, version)
         files = {
             'photo': file,
         }
         response = requests.post(url, files=files)
-        file.close()
         response.raise_for_status()
-        uploading_photo = [response.json()['server'], response.json()['photo'], response.json()['hash']]
-        return uploading_photo
+    uploading_photo = response.json()
+    return uploading_photo
 
 
-def save_wall_photo(access_token, group_id, uploading_photo):
-    payload = {"group_id": f"{group_id}", "photo": f"{uploading_photo[1]}", "server": f"{uploading_photo[0]}",
-               "hash": f"{uploading_photo[2]}", "access_token": f"{access_token}", "v": "5.131"}
+def save_wall_photo(access_token, group_id, version, server, photo, photo_hash):
+    payload = {"group_id": {group_id}, "photo": {photo}, "server": {server},
+               "hash": {photo_hash}, "access_token": {access_token}, "v": {version}}
     response = requests.post('https://api.vk.com/method/photos.saveWallPhoto', params=payload)
     response.raise_for_status()
     return response.json()['response'][0]
 
 
-def post_comic_on_wall(access_token, group_id, info_image, message):
-    owner_id = info_image['owner_id']
-    media_id = info_image['id']
+def post_comic_on_wall(access_token, group_id, version, owner_id, media_id, message):
     attachments = f'photo{owner_id}_{media_id}'
-    payload = {"owner_id": f"{-group_id}", "group_id": f"{group_id}", "from_group": True,
-               "attachments": f"{attachments}", "message": f"{message}", "access_token": f"{access_token}",
-               "v": "5.131"}
+    payload = {"owner_id": {-group_id}, "group_id": {group_id}, "from_group": True,
+               "attachments": {attachments}, "message": {message}, "access_token": {access_token},
+               "v": {version}}
     response = requests.post('https://api.vk.com/method/wall.post', params=payload)
     response.raise_for_status()
     return response.json()
@@ -63,19 +53,24 @@ def main():
     load_dotenv()
     access_token = os.environ['VK_ACCESS_TOKEN']
     group_id = int(os.environ['VK_GROUP_ID'])
+    version = '5.131'
     number_last_comic = 2750
     comic_info = get_comic_photo(number_last_comic)
     try:
-        get_response_api_vk(access_token)
         download_comic(comic_info['img'], 'image.png')
-        uploading_photo = upload_photos_to_server(access_token, group_id)
-        save_photo = save_wall_photo(access_token, group_id, uploading_photo)
-        post_comic_on_wall(access_token, group_id, save_photo, comic_info['alt'])
+        uploading_photo = upload_photos_to_server(access_token, group_id, version)
+        server = uploading_photo['server']
+        photo = uploading_photo['photo']
+        photo_hash = uploading_photo['hash']
+        save_photo = save_wall_photo(access_token, group_id, version, server, photo, photo_hash)
+        owner_id = save_photo['owner_id']
+        media_id = save_photo['id']
+        print(owner_id, media_id)
+        post_comic_on_wall(access_token, group_id, version, owner_id, media_id, comic_info['alt'])
     except ValueError:
         print("Environment variables error!")
     finally:
-        print("Comics uploaded to the wall of the VK group!")
-    os.remove('image.png')
+        os.remove('image.png')
 
 
 if __name__ == '__main__':
